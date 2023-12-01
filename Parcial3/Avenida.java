177d0f28cd1e6f3bd2f3a140dca94c0b445b9969
patrickvillarroel <130080309+patrickvillarroel@users.noreply.github.com>
2023-12-01 14:28:41 -0500

    Add files via upload

---
+++ Parcial3/Avenida.java
@@ -0,0 +1,90 @@
+import java.io.*;
+
+class Interseccion {
+    private int[][] arr;
+    private int calle, avenida;
+
+    // Constructor y metodos
+    public Interseccion(int f, int c) {
+        arr = new int[f][c];
+
+    }
+
+    public void asignarDatos(int c, int a) {
+        calle = c;
+        avenida = a;
+    }
+
+    public int devolverCalle() {
+        return calle;
+    }
+
+    public int devolverAvenida() {
+        return avenida;
+    }
+
+    public int SumarElemento() {
+        arr[(avenida - 1)][(calle - 1)]++;
+        return arr[(avenida - 1)][(calle - 1)];
+    }
+
+    public int sumarTodos() {
+        int suma = 0;
+        int i, j;
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 10; j++) {
+                suma += arr[i][j];
+            }
+        }
+        return suma;
+    }
+
+    public int sumarFilas(int k) {
+        int i, j;
+        int suma[] = new int[arr.length];
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 10; j++) {
+                suma[i] += arr[i][j];
+            }
+        }
+        return suma[k];
+    }
+
+    public int mostrar_elemento(int f, int c) {
+        return arr[f][c];
+    }
+
+}
+
+public class Avenida {
+    public static void main(String args[]) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int i, j, calle = 0, avenida = 0, opcion = 0;
+        Interseccion obj = new Interseccion(3, 10);
+
+        try {
+            System.out.println("Cuantos accidentes desea registrar?");
+            opcion = Integer.parseInt(br.readLine());
+            for (i = 0; i < opcion; i++) {
+                System.out.println("En donde ocurrio el accidente?");
+                System.out.println("Avenida: ");
+                avenida = Integer.parseInt(br.readLine());
+                System.out.println("Calle: ");
+                calle = Integer.parseInt(br.readLine());
+                obj.asignarDatos(calle, avenida);
+                obj.SumarElemento();
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Error de entrada");
+        }
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 10; j++) {
+                System.out.println("Elemento " + (i + 1) + "x" + (j + 1) + ": " + obj.mostrar_elemento(i, j));
+            }
+        }
+        for (int k = 1; k <= 3; k++) {
+            System.out.println("\nLa suma de la fila " + k + " es: " + obj.sumarFilas(k - 1));
+        }
+    }
+}
